094a090e588c0f5b79343f2d1432457d87198d3a
Jaloliddin-Zokirov <jaloliddinzokirov.dev@gmail.com>
2023-01-07 23:05:17 +0500

    Add editor product section

---
+++ js/main.js
@@ -0,0 +1,78 @@
+const elCards = document.querySelector('.js-cards')
+
+const localCards = window.localStorage.getItem(`cards`);
+
+let cards = localCards ? JSON.parse(localCards) : [];
+
+async function getCards(url) {
+  try {
+    const rawData = await fetch(`https://fakestoreapi.com/${url}`);
+    const data = await rawData.json();
+    cards = data;
+    window.localStorage.setItem(`cards`, JSON.stringify(data));
+  } catch (error) {
+    console.error(error);
+    console.error(`Internet uzildi`);
+  }
+}
+
+getCards('products')
+
+
+function renderArr(data) {
+  elCards.innerHTML = ''
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+    let newItem = document.createElement('li')
+    newItem.classList.add('product__cards-item')
+
+    let newImg = document.createElement('img')
+    newImg.src = element.image
+
+    let newDiv = document.createElement('div')
+    newDiv.classList.add('product__content')
+
+    let newText = document.createElement('p')
+    newText.textContent = element.title
+    newText.classList.add('product__text')
+
+    let newRate = document.createElement('div')
+    newRate.classList.add('product__star')
+
+    let newRateOne = document.createElement('p')
+    newRateOne.className = `fa fa-star ${Math.round(+element.rating.rate) >= 1 ? 'checked' : ''}`
+
+    let newRateTwo = document.createElement('p')
+    newRateTwo.className = `fa fa-star ${Math.round(+element.rating.rate) >= 2 ? 'checked' : ''}` 
+    
+    let newRateThere = document.createElement('p')
+    newRateThere.className = `fa fa-star ${Math.round(+element.rating.rate) >= 3 ? 'checked' : ''}`
+    
+    let newRateFour = document.createElement('p')
+    newRateFour.className = `fa fa-star ${Math.round(+element.rating.rate) >= 4 ? 'checked' : ''}` 
+    
+    let newRateFive = document.createElement('p')
+    newRateFive.className = `fa fa-star ${Math.round(+element.rating.rate) >= 5 ? 'checked' : ''}`
+
+    let newPriceBox = document.createElement('div')
+
+    let newPrice = document.createElement('span')
+    newPrice.textContent = (+element.price * 0.76).toFixed(2)
+    newPrice.classList.add('product__price')
+
+    let newPriceDel = document.createElement('del')
+    newPriceDel.textContent = `$${element.price}`
+    newPriceDel.classList.add('product__price-del')
+
+    let newPricePercentage = document.createElement('span')
+    newPricePercentage.textContent = '24% Off'
+    newPricePercentage.classList.add('product__percentage')
+
+    newPriceBox.append(newPrice, newPriceDel, newPricePercentage)
+    newRate.append(newRateOne, newRateTwo, newRateThere, newRateFour, newRateFive)
+    newDiv.append(newText, newRate, newPriceBox)
+    newItem.append(newImg, newDiv)
+    elCards.appendChild(newItem)
+  }
+}
+renderArr(cards)
